79a77c22753979172ebbf6c9409665bd1f3dfe2f
Pajehome <abdel20069@gmail.com>
2022-05-12 15:52:18 +0200

    card

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -2,15 +2,15 @@
   <div>
       
          <li>
-          id: {{card.id}}<br />
-          titolo originale: {{card.original_title ? card.original_title : card.original_name}}<br />
-          titolo: {{card.title ? card.title : card.name}}<br />
-          lingua: {{card.original_language}}<br />
-         <country-flag :country='card.original_language' size='small'/> <br />
-          voto: {{rounds(card.vote_average)}}
-          <img :src="image+card.backdrop_path" alt="">
+          id: {{item.id}}<br />
+          titolo originale: {{item.original_title ? item.original_title : item.original_name}}<br />
+          titolo: {{item.title ? item.title : item.name}}<br />
+          lingua: {{item.original_language}}<br />
+         <country-flag :country='item.original_language' size='small'/> <br />
+          voto: {{rounds(item.vote_average)}}
+          <img :src="image+item.backdrop_path" alt="">
           <span v-for="(numero, index) in 5" :key="index">
-              <i :class="numero <= rounds(card.vote_average) ? 'fa-solid fa-star yellow' : 'fa-solid fa-star'"></i>
+              <i :class="numero <= rounds(item.vote_average) ? 'fa-solid fa-star yellow' : 'fa-solid fa-star'"></i>
           </span>
         </li>
       
@@ -28,7 +28,7 @@ data(){
 
 },
 props:{
-        card: Object,
+        item: Object,
         images: String,
         
     },
